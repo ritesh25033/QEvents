6f7a54dda9e35e98c02a2f91765654bf55487820
ritesh <ritesh25033@gmail.com>
2024-11-26 12:02:22 +0530

    testing

--- app/tags/page.js
+++ app/tags/page.js
@@ -1,11 +1,12 @@
-import Tag from "@/components/Tag";
+import Tag from '@/components/Tag';
 
 export default async function TagsPage() {
-  const apiResponse = await fetch("https://qevent-backend.labs.crio.do/tags");
+  const apiResponse = await fetch('https://qevent-backend.labs.crio.do/tags');
   const tags = await apiResponse.json();
+
   return (
-    <div className="flex flex-row justify-center items-center h-[50vh] px-4">
-      <div className="flex flex-row justify-center items-center flex-wrap gap-x-2 gap-y-2 h-[30px] w-[900px]">
+    <div className='flex flex-row justify-center items-center h-[50vh] px-4'>
+      <div className='flex flex-row justify-center items-center flex-wrap gap-x-2 gap-y-2 h-[30px] w-[900px]'>
         {tags.map((tag) => (
           <div key={tag.id}>
             <Tag text={tag.name} />
